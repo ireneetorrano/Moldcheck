47d2e1af194dffbc2e5402b27ea3c9e69f003fc1
Irene Hernández Torrano <irene.vtorrano@gmail.com>
2026-05-06 15:30:54 +0100

    Add Google Analytics to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "@/app/globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
@@ -15,6 +16,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RB2NEE6FTK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RB2NEE6FTK');`}
+        </Script>
+      </head>
       <body>
         {children}<SpeedInsights />
       </body>
